5502bb2f0d152ef455d37c8c8584dca8a42672d9
Saleque474 <salequeahmed474@gmail.com>
2023-12-22 22:35:17 +0600

    cors added

--- configurations/settings.py
+++ configurations/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2d-&_h#4f50yfw6yoh4dp#d6i)ev@)8(=81f50bxzx$^h7c^bb
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CORS_ALLOWED_ORIGINS = ['*']
 
 # Application definition
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "api",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'api.middleware.CustomRequestMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'configurations.urls'
